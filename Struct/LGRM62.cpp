d54891f24f8de2b40c9a8c8ae07dc704d846a3c5
Luana Macedo <92169736+luanagbrm@users.noreply.github.com>
2024-06-09 11:22:42 -0300

    Update LGRM62.cpp

--- Struct/LGRM62.cpp
+++ Struct/LGRM62.cpp
@@ -15,39 +15,34 @@ void fGrupo(struct stamigo am[50], int i);
 void fclear();
 
 struct stamigo {
-		char nome[40];
-		char telefone[12];
-		int idade;
-	};
+	char nome[40];
+	char telefone[12];
+	int idade;
+};
 
 int main(){
-	
 	int i;
 	
 struct stamigo amigos[50];
 	do {
-		
-			printf("\nNome %d (ENTER para sair): ", i+1);
-			gets(amigos[i].nome);
-			if(amigos[i].nome[0] == '\0')
-				break;
-			printf("\nTelefone %d: ", i+1);
-			gets(amigos[i].telefone);
-			printf("\nIdade %d: ", i+1);
-			scanf("%d", &amigos[i].idade);
-			i++;
+		printf("\nNome %d (ENTER para sair): ", i+1);
+		gets(amigos[i].nome);
+		if(amigos[i].nome[0] == '\0')
+			break;
+		printf("\nTelefone %d: ", i+1);
+		gets(amigos[i].telefone);
+		printf("\nIdade %d: ", i+1);
+		scanf("%d", &amigos[i].idade);
+		i++;
 			
-			fclear();
+		fclear();
 	
 	} while(i < 50);
 	
 	fGrupo(amigos, i);
-	
-
 	return 0;	
 } 
 void fGrupo(stamigo amigos[50], int i){
-	
 	system("cls");
 	printf("\nGrupo dos %d amigos\n", i);
 	printf("---------------------------------------------\n");
